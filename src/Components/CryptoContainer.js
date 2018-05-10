4f5b76489fa9dd3f934be87440e75e6fe87cb6dc
Scott <scrosehart@gmail.com>
2018-05-09 17:55:08 -0700

    most of boiler plate finished

---
+++ src/Components/CryptoContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View, Text } from 'react-native';
+
+import { FetchCoinData } from '../actions/FetchCoinData';
+
+class CryptoContainer extends Component {
+
+  componentDidMount() {
+    this.props.FetchCoinData();
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Container</Text>
+      </View>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    crypto: state.cyrpto
+  }
+}
+
+export default connect(mapStateToProps, { FetchCoinData })(CryptoContainer)
